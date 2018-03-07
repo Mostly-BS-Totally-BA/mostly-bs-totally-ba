01774050ca4ad452c33c37872cc83c81629eab13
Christian Wilson <36175579+crw24@users.noreply.github.com>
2018-03-07 12:47:23 -0500

    Advanced Door Opening
    Doors open and make new game objects appear instead of just disappearing from screen

--- CSE442_Project/Assets/Scripts/Interaction/Door_Interact.cs
+++ CSE442_Project/Assets/Scripts/Interaction/Door_Interact.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Door_Interact : MonoBehaviour
 {
+    public GameObject opened_door;
     public void RunInteraction()
     {
         gameObject.SetActive(false);
+        if (opened_door != null)
+        {
+            opened_door.SetActive(true);
+        }
     }
 }
  
