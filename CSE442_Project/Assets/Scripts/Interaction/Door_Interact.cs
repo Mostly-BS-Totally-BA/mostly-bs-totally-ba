45c4bb44d48a34cd7c6f4894c5be1dc253c01e36
Christian Wilson <36175579+crw24@users.noreply.github.com>
2018-03-06 22:58:46 -0500

    Added Scripts
    Added scripts and altered doors

---
+++ CSE442_Project/Assets/Scripts/Interaction/Door_Interact.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Interact : MonoBehaviour
+{
+    public void RunInteraction()
+    {
+        gameObject.SetActive(false);
+    }
+}
+ 
---
+++ CSE442_Project/Assets/Scripts/Interaction/Player_Interact.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Interact : MonoBehaviour
+{
+
+    public GameObject interactingObj = null;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Door_Interactable"))  
+        {
+            interactingObj = collision.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Door_Interactable"))
+        {
+            if (collision.gameObject == interactingObj)
+            {
+                interactingObj = null;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Interaction") && interactingObj)
+        {
+            interactingObj.SendMessage("RunInteraction");
+        }
+    }
+}
+
+
