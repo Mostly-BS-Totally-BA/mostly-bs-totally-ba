353a3eae09cb83bcc7b4021cc2e549ead3f81ea7
Christian Wilson <36175579+crw24@users.noreply.github.com>
2018-03-10 17:13:30 -0500

    lvl 2 Door Interaction
    Scripted Door opening for lvl 2, modified walls for improved collision, placed overlays to black out rooms not yet discovered

---
+++ CSE442_Project/Assets/Scripts/Interaction/Double_Door_Interact.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Double_Door_Interact : MonoBehaviour {
+
+    public GameObject opened_door1;
+    public GameObject opened_door2;
+    public GameObject other_door;
+    public GameObject [] overlay;
+    int number_of_Overlays;
+
+    public void RunInteraction()
+    {
+        number_of_Overlays = overlay.GetLength(0);
+        for (int i = 0; i < number_of_Overlays; i++)
+            overlay[i].SetActive(false);
+
+        gameObject.SetActive(false);
+        other_door.SetActive(false);
+        opened_door1.SetActive(true);
+        opened_door2.SetActive(true);
+    }
+}
