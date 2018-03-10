457e50c4f63efebec308db9d29e857bb22840018
Daniel <dere@buffalo.edu>
2018-03-10 13:57:42 -0500

    Added Attacking
    I have implemented player attacking the enemy, as well as the corresponding attack animations and button click (space)

---
+++ CSE442_Project/Assets/Scripts/Player Scripts/PlayerAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D _enemy)
+	{
+		GameObject enemy = _enemy.gameObject;
+		if (_enemy.gameObject.CompareTag("Enemy")) {
+			enemy.GetComponent<Enemy_movement> ().takeDamage (50);
+		}
+
+	}
+}
+
