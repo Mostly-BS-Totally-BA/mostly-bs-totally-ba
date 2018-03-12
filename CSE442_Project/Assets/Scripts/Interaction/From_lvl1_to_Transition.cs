0b5362853ed5d22e954bc4ad1d0c70fca2749f77
Christian Wilson <36175579+crw24@users.noreply.github.com>
2018-03-11 21:11:17 -0400

    Attempt to fix merge conflict
    attempting to fix merge conflicts

---
+++ CSE442_Project/Assets/Scripts/Interaction/From_lvl1_to_Transition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class From_lvl1_to_Transition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
